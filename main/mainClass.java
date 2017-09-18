4ce439ff122486d87c73d07f2d906afd6b98a406
ManiaK974 <32074090+ManiaK974@users.noreply.github.com>
2017-09-18 20:45:49 +0200

    Ajout du dossier "Main" du projet java

---
+++ main/mainClass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import views.Splash_view;
+
+/**
+ *
+ * @author Maniak
+ */
+public class mainClass {
+
+    /**
+     * Lance le premier élément du programme Solve It
+     *
+     * @param args : String[] arguments par défaut de la méthode main()
+     */
+    public static void main(String[] args) {
+        new Splash_view();
+    }
+}
